08f3e9cc9d4fa847ea7bebfdcf6b30a70ff08ee4
clintonjosephs <clintonjosephs26@gmail.com>
2022-10-04 23:49:04 +0000

    Attempt progress bar

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,5 +1,8 @@
 const menuOverlay = document.querySelector('#navigation');
 const mobileMenuButton = document.querySelector('#mobileMenuButton');
+const dateShow = document.querySelector('.dateShow');
+
+dateShow.textContent = new Date().getFullYear();
 
 const toogleMenu = () => {
   menuOverlay.style.display = 'block';
@@ -19,3 +22,6 @@ for (let i = 0; i < mobileMenuLinks.length; i += 1) {
     closeMobileMenu();
   });
 }
+
+
+
