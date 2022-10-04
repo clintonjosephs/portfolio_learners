d16bb5bc081bf681aab7fc0659892d828c931792
clintonjosephs <clintonjosephs26@gmail.com>
2022-10-04 14:13:29 +0000

    Changed the size of the cards width

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,19 +1,21 @@
-const menuOverlay = document.querySelector("#navigation");
-const mobileMenuButton = document.querySelector("#mobileMenuButton");
+const menuOverlay = document.querySelector('#navigation');
+const mobileMenuButton = document.querySelector('#mobileMenuButton');
 
 const toogleMenu = () => {
-    menuOverlay.style.display = 'block';
-}
+  menuOverlay.style.display = 'block';
+};
 
 const closeMobileMenu = () => {
+  if (window.innerWidth < 992) {
     menuOverlay.style.display = 'none';
-}
+  }
+};
 
 mobileMenuButton.addEventListener('click', toogleMenu);
 
-const mobileMenuLinks = document.querySelectorAll(".navigation li a");
+const mobileMenuLinks = document.querySelectorAll('.navigation li a');
 for (let i = 0; i < mobileMenuLinks.length; i += 1) {
-  mobileMenuLinks[i].addEventListener("click", () => {
+  mobileMenuLinks[i].addEventListener('click', () => {
     closeMobileMenu();
   });
 }
